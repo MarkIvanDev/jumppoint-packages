b4378934e9797304cf2e573762cd35599f52ccb8
Mark Ivan Basto <markivan.developer@outlook.com>
2022-07-08 14:43:04 +0800

    add NewItem extension support

---
+++ JumpPoint.Packages/JumpPoint.Extensions/NewItems/NewItemHelper.shared.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpPoint.Extensions.NewItems
+{
+    public static partial class NewItemHelper
+    {
+        public static IDictionary<string, object> GetData(NewItemPayload payload)
+        {
+            var data = new Dictionary<string, object>();
+            data.Add(nameof(NewItemPayload.Destination), payload.Destination);
+            return data;
+        }
+
+        public static NewItemResultPayload GetResult(IDictionary<string, object> data)
+        {
+            if (data.TryGetValue(nameof(NewItemResultPayload.FileName), out var fileName))
+            {
+                return new NewItemResultPayload
+                {
+                    FileName = fileName?.ToString(),
+                    ContentToken = data.TryGetValue(nameof(NewItemResultPayload.ContentToken), out var contentToken) ? contentToken?.ToString() : null
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ JumpPoint.Packages/JumpPoint.Extensions/NewItems/NewItemResultPayload.shared.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JumpPoint.Extensions.NewItems
+{
+    public class NewItemResultPayload
+    {
+        public string FileName { get; set; }
+
+        public string ContentToken { get; set; }
+    }
+}
